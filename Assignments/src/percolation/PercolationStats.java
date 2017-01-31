94ccae20d7ed37e861c1794d9e0244569071f057
samson <samsonbrimnelson@gmail.com>
2017-01-30 18:55:36 -0700

    Percolation Stats DONE

--- Assignments/src/percolation/PercolationStats.java
+++ Assignments/src/percolation/PercolationStats.java
@@ -1,4 +1,9 @@
 package percolation;
+
+import edu.princeton.cs.introcs.StdOut;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 /**
  * 
  * @author Samson Nelson
@@ -8,33 +13,64 @@ package percolation;
  */
 
 public class PercolationStats {
+	
+	private int experimentsCount;
+    private Percolation pr;
+    private double[] fractions;
 
 	// perform trials independent experiments on an n-by-n grid
-	public PercolationStats(int n, int trials) {
+	public PercolationStats(int n, int t) {
+		
+		if (n <= 0 || t <= 0) {
+            throw new IllegalArgumentException("Given n <= 0 || t <= 0");
+        }
+		
+		experimentsCount = t;
+        fractions = new double[experimentsCount];
+        for (int expNum = 0; expNum < experimentsCount; expNum++) {
+            pr = new Percolation(n);
+            int openedSites = 0;
+            while (!pr.percolates()) {
+                int x = StdRandom.uniform(0, n);
+                int y = StdRandom.uniform(0, n);
+                if (!pr.isOpen(x, y)) {
+                    pr.open(x, y);
+                    openedSites++;
+                }
+            }
+            double fraction = (double) openedSites / (n * n);
+            fractions[expNum] = fraction;
+        }
 		
 	}   
 	//sample mean of percolation threshold
 	   public double mean() {
-		return 0;
+		  return StdStats.mean(fractions);
 	}            
 	   
 	//sample standard deviation of percolation threshold
 	   public double stddev() {
-		return 0;
+		  return StdStats.stddev(fractions);
 	}              
 	   
 	//low  endpoint of 95% confidence interval
 	   public double confidenceLo() {
-		return 0;
+		   return mean() - ((1.96 * stddev()) / Math.sqrt(experimentsCount));
 	}               
 	   
 	//high endpoint of 95% confidence interval
 	   public double confidenceHi() {
-		return 0;
+		   return mean() + ((1.96 * stddev()) / Math.sqrt(experimentsCount));
 	}                
 
 	   public static void main(String[] args) {
-		   String filename = args[0];
-		   System.out.println("Hello " + filename + " you are the best there every was");
+		   int N = Integer.parseInt(args[0]);
+	        int T = Integer.parseInt(args[1]);
+	        PercolationStats ps = new PercolationStats(N, T);
+
+	        StdOut.println("mean                    = " + ps.mean());
+	        StdOut.println("stddev                  = " + ps.stddev());
+	        StdOut.println("confindenceLow          = " + ps.confidenceLo());
+	        StdOut.println("confindenceHigh         = " + ps.confidenceHi());
 	   } 
 }
